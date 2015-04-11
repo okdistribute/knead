0d3acaf611b6cf75e09c0e783d6789660b355294
Karissa McKelvey <krmckelv@gmail.com>
2015-04-11 00:25:23 -0700

    CLN: remove comments/console.log

--- index.js
+++ index.js
@@ -1,58 +1,22 @@
 var dat = require('dat-core')
-var daff = require('daff')
 var debug = require('debug')('daff.visualdiff')
 var batcher = require('byte-stream')
 var through = require('through2')
 
-var NdjsonTable = require('./lib/ndjson_table_view.js')
+var toDaff = require('./lib/dat2daf.js')
 
 module.exports = {}
 
-var flags = new daff.CompareFlags();
-flags.allow_nested_cells = true;
 
 function visualdiff(diffStream, opts, cb) {
   var batchedStream = batcher(10)
   diffStream
     .pipe(batchedStream)
     .pipe(through.obj(function (data, enc, next) {
-      console.log(data)
       toDaff(data, opts, function (daff) {
         cb(null, daff)
       })
     })
   )
 }
-
-function toDaff (changes, opts, cb) {
-  if (!opts) opts = {}
-
-  var table1 = new NdjsonTable(changes, function(row) {
-    if (row && row[0]) return row[0]["value"]
-    else return
-  });
-
-  var table2 = new NdjsonTable(changes, function(row) {
-    if (row && row[1]) return row[1]["value"]
-    else return
-  });
-
-  var alignment = daff.compareTables(table1, table2, flags).align();
-  var highlighter = new daff.TableDiff(alignment,flags);
-  var table_diff = new daff.SimpleTable();
-  highlighter.hilite(table_diff);
-
-  if (opts.html) {
-    var diff2html = new daff.DiffRender();
-    diff2html.render(table_diff);
-    var table_diff_html = diff2html.html();
-    return cb(table_diff_html)
-  }
-  else {
-    var diff2terminal = new daff.TerminalDiffRender();
-    diff2terminal.render(table_diff)
-    return cb(table_diff)
-  }
-}
-
 module.exports = visualdiff
