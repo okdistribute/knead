3cd519e571fe20eb7766b1734f47f90d16734b5b
Karissa McKelvey <krmckelv@gmail.com>
2015-04-21 00:25:36 -0700

    start some modularization

--- test/test.js
+++ test/test.js
@@ -7,13 +7,9 @@ var memdb = require('memdb')
 var createDatConflicts = require('./createDatConflicts.js')
 var dat2daff = require('../lib/dat2daff.js')
 var visualdiff = require('../index.js')
-var testData = require('./testData.js')
+var DATA = require('test-data')
 
-var TABLES = [
-  testData.COUNTRIES_0,
-  testData.COUNTRIES_1,
-  testData.COUNTRIES_2
-]
+TABLES = DATA.CONFLICTS.SMALL
 
 test('visualdiff terminal', function (t) {
   var db = dat(memdb(), {valueEncoding: 'json'})
@@ -58,52 +54,6 @@ test('visualdiff older switches with head switch', function (t) {
       t.equals(data.older, 'right')
       t.equals(table1.height, 3)
       t.equals(table2.height, 4)
-      t.deepEquals(table1.columns, ['capital', 'country'])
-      t.deepEquals(table2.columns, ['capital', 'code', 'country'])
-      t.same(typeof(visual), 'string')
-      t.same(typeof(next), 'function')
-      t.end()
-    })
-  })
-})
-
-
-test('dat2daff.fromReadStreams with limit', function (t) {
-  var db = dat(memdb(), {valueEncoding: 'json'})
-  createDatConflicts(db, TABLES, function (heads) {
-    var stream1 = db.checkout(heads[0]).createReadStream()
-    var stream2 = db.checkout(heads[1]).createReadStream()
-    var opts = {
-      html: false,
-      limit: 20
-    }
-    dat2daff.fromReadStreams(stream1, stream2, opts, function (tables, visual, next) {
-      var table1 = tables[0]
-      var table2 = tables[1]
-      t.equals(table1.height, 5)
-      t.equals(table2.height, 12)
-      t.deepEquals(table1.columns, ['capital', 'country'])
-      t.deepEquals(table2.columns, ['capital', 'code', 'country'])
-      t.same(typeof(visual), 'string')
-      t.same(typeof(next), 'function')
-      t.end()
-    })
-  })
-})
-
-test('dat2daff.fromReadStreams without limit should default to 50', function (t) {
-  var db = dat(memdb(), {valueEncoding: 'json'})
-  createDatConflicts(db, TABLES, function (heads) {
-    var stream1 = db.checkout(heads[0]).createReadStream()
-    var stream2 = db.checkout(heads[1]).createReadStream()
-    var opts = {
-      html: false,
-    }
-    dat2daff.fromReadStreams(stream1, stream2, opts, function (tables, visual, next) {
-      var table1 = tables[0]
-      var table2 = tables[1]
-      t.equals(table1.height, 5)
-      t.equals(table2.height, 12)
       t.deepEquals(table1.columns, ['capital', 'country'])
       t.deepEquals(table2.columns, ['capital', 'code', 'country'])
       t.same(typeof(visual), 'string')
