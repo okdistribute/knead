f47ca2c5740709244f781e818042a1a6e77553f5
Karissa McKelvey <krmckelv@gmail.com>
2015-04-22 10:58:16 -0700

    Add ability to merge two files into a single file

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var argv = require('minimist')(process.argv.slice(2))
+var detect = require('detect-data-stream')
+var formatData = require('format-data')
 var diff = require('sorted-diff-stream')
 var fs = require('fs')
 var batcher = require('byte-stream')
@@ -11,11 +13,30 @@ if (argv._.length !== 3) {
   process.exit()
 }
 
-var base = fs.createReadStream(argv[0])
-var newfile = fs.createReadStream(argv[1])
-var diffStream = diff(base, newfile)
-
+var localPath = argv._[0]
+var remotePath = argv._[1]
+var outPath = argv._[2]
+var format = argv.format || 'csv'
 var limit = (argv.limit || 20) * 2
+
+if (fs.existsSync(outPath)) {
+  console.log(outPath, 'exists. Appending to end of file.')
+}
+else {
+  console.log('Creating new file', outPath)
+}
+
+var localFile = fs.createReadStream(localPath).pipe(detect())
+var newFile = fs.createReadStream(remotePath).pipe(detect())
+var outFile = fs.createWriteStream(outPath, {flags: 'a'})
+
+function jsonEquals (a, b, cb) {
+  if (JSON.stringify(a) === JSON.stringify(b)) cb(null, true)
+  else cb(null, false)
+}
+
+var diffStream = diff(localFile, newFile, jsonEquals)
+
 var batchStream = batcher(limit)
 
 var opts = {
@@ -24,10 +45,18 @@ var opts = {
 }
 var doughStream = dough(opts)
 
-var outStream = diffStream.pipe(batchStream).pipe(doughStream)
-outStream.on('data', console.log)
+var outStream = diffStream.pipe(batchStream).pipe(doughStream).pipe(formatData(format))
 
+outStream.on('data', function (data) {
+  outFile.write(data)
+})
+
+outStream.on('end', function () {
+  localFile.end()
+  newFile.end()
+  outFile.end()
+})
 
 function usage () {
-  console.log('dough <basefile> <newfile> <outfile> [--limit <num>]')
+  console.log('dough <basefile> <newfile> <outfile> [--format csv,ndjson,json] [--limit <num>]')
 }
