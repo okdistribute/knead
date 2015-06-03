b4f6de2174edacca9aa328a7244db33a444517ad
Karissa McKelvey <krmckelv@gmail.com>
2015-06-03 11:53:30 -0700

    Fix cli

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ var manualMergeStream = require('manual-merge-stream')
 var diff2daff = require('diff2daff')
 
 module.exports = function (diffStream, opts, merge) {
-  if (!merge) {
-    merge = opts
+  if (!opts) {
     opts = {}
   }
-  var batchStream = Batcher((opts.limit || 20) * 2)
+  var limit = (opts.limit || 20) * 2
+  var batchStream = Batcher(limit)
   var vizFn = opts.vizFn || diff2daff
 
-  return diffStream.pipe(batchStream).pipe(manualMergeStream(vizFn, merge))
+  var mergeStream
+  if (merge) mergeStream = manualMergeStream(vizFn, merge)
+  else mergeStream = manualMergeStream(vizFn)
+
+  return diffStream.pipe(batchStream).pipe(mergeStream)
 }
